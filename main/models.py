b1d3a331d1a6e7e4823a53bb092ae208c948b069
alessandragaleno <alessandragaleno421@gmail.com>
2024-06-07 18:49:06 -0300

    primeiro comite

---
+++ main/models.py
@@ -0,0 +1,16 @@
+
+from django.db import models
+
+
+# Create your models here.
+class Todo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE,related_name="items")
+    text = models.TextField("descrição de item")
+    complete = models.BooleanField(default=False)
---
+++ settings/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from main.views import index, show, store, destroy, destroy2, put
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',index,name='index'),
+    path("<int:id>/", show, name='show'),
+    path('nova/tarefa/', store, name='store'),
+    path('<int:todo_id>/novo/item/', store, name='store-item'),
+    path('<int:todo_id>/excluir/tarefa/', destroy, name="destroy"),
+    path('<int:todo_id>/<int:item_id>/excluir/item/', destroy2, name="destroy2"),
+    path('item/<int:item_id>/edit/', put, name='put-item'),
+]
